923beb6ca5bb66af90d1f352e55b14ea29684340
Silas Kraume <silas.kraume1552@gmail.com>
2025-02-08 20:01:22 +0100

    thesis presentation

---
+++ BachelorThesis/Presentation/softlocka.cpp
@@ -0,0 +1,10 @@
+
+
+while (true) {
+    { // wait for threads to finish
+    std::lock_guard<std::mutex> threads_guard(running_threads_mutex);
+    if (running_threads.size() == 0)
+        break;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+}
